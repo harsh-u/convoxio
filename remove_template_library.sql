67fd28d222a5cabb0b5faa25232afc59388a08dc
Harsh Raj <harsh.raj@screen-magic.com>
2025-08-10 19:37:39 +0530

    first-prod-draft

--- remove_template_library.sql
+++
@@ -1,7 +0,0 @@
--- Remove template_library table and functionality
--- Run this SQL script to clean up the database
-
--- Drop the template_library table
-DROP TABLE IF EXISTS template_library;
-
--- The template table remains unchanged and continues to be used for user templates
